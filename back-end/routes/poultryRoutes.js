8cc8f706cebe6462ebd55397565a42b09c98178e
Johnson11 <kalijoe11@gmail.com>
2024-09-29 16:34:47 +0300

    update pipeline

--- back-end/routes/poultryRoutes.js
+++ back-end/routes/poultryRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const poultryController = require('../controllers/poultryController');
 
 router.post('/poultry', poultryController.createRecord);
+
 router.get('/poultry', poultryController.getRecords);
 
 module.exports = router;

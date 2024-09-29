4b06c520e65345faf1116953daca52e05830fbfe
Johnson11 <kalijoe11@gmail.com>
2024-09-29 17:14:04 +0300

    update pipeline

--- back-end/app.js
+++ back-end/app.js
@@ -11,12 +11,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 app.use("/api", poultryRoutes);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
 app.listen(port, () => console.log('Listening on port ${port}'));

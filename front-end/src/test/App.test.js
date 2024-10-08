987a2b376b8ea74cfc37c141b463df37f61cece2
tusiimirwe.jemily <tusiimirwe.jemily@students.mak.ac.ug>
2024-10-08 18:47:12 +0300

    Made change in App.test.js

--- front-end/src/test/App.test.js
+++ front-end/src/test/App.test.js
@@ -2,12 +2,12 @@ import multiply from "../hooks/multiply.js";
 import { assert } from "chai"; 
 
 describe("Multiply Function tests", () => {
-  it("should return 1 when multiplying 1 and 1", () => {
-    assert.equal(multiply(1, 1), 1);
+  it("should return 9 when multiplying 3 and 3", () => {
+    assert.equal(multiply(3, 3), 9);
   });
 
-  it("should return 4 when multiplying 2 and 2", () => {
-    assert.equal(multiply(2, 2), 4);
+  it("should return 25 when multiplying 5 and 5", () => {
+    assert.equal(multiply(5, 5), 25);
   });
 
   it("should return 9 when multiplying 3 and 3", () => {

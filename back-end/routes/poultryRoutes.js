d8657d986ec32b2fbe1a988f6d5e6bde87c20c11
tusiimirwe.jemily <tusiimirwe.jemily@students.mak.ac.ug>
2024-10-14 23:43:34 +0300

    Improved on the code readability

--- back-end/routes/poultryRoutes.js
+++ back-end/routes/poultryRoutes.js
@@ -1,9 +1,11 @@
+// Import the express module
 const express = require('express');
 const router = express.Router();
+// Import the poultryController from the controllers directory
 const poultryController = require('../controllers/poultryController');
-
+// Route to create a new poultry record (POST request)
 router.post('/', poultryController.createRecord);
-
+// Route to retrieve all poultry records (GET request)
 router.get('/', poultryController.getRecords);
-
+// Export the router object to be used in other parts of the application
 module.exports = router;
